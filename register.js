04e22c6e05ed0fbb44c52c9d21bb6d7c7e8e6273
Kajsa.Soderlund <kajsasoderlund99@gmail.com>
2024-02-21 13:37:15 +0100

    New js file
    Co-authored-by: TmyVin <TmyVin@users.noreply.github.com>
    Co-authored-by: Niklas Sörén Högblom <sh-niklas@users.noreply.github.com>
    Co-authored-by: CarolineAlmgren <CarolineAlmgren@users.noreply.github.com>

---
+++ register.js
@@ -0,0 +1,39 @@
+let registrationForm = document.getElementById("registrationForm")
+let registerUsername = document.getElementById("registerUsername")
+let registerPassword = document.getElementById("registerPassword")
+let registerPasswordCheck = document.getElementById("registerPasswordCheck")
+let errorMessage = document.getElementById("errorMessage")
+
+
+
+registrationForm.addEventListener("submit", async (ev)=>{
+    ev.preventDefault();
+    if (registerPassword.value == registerPasswordCheck.value){
+  
+      console.log("hej");
+      const rawResponse = await fetch('http://localhost:3000/api/useraccount',{
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+          },
+          method:'POST',
+          credentials:'include',
+          body: JSON.stringify({Username: registerUsername.value,password:registerPassword.value})                
+      })
+      console.log(rawResponse);
+      if(rawResponse.status == 204){
+        //const content = await rawResponse.json();
+        window.location.replace('index.html');
+      }
+      else{
+        errorMessage.style.display = "block";
+        errorMessage.innerHTML = "Något gick fel"
+    }
+  
+    }
+    else {
+      errorMessage.style.display = "block";
+    }
+  })
+  
+    
